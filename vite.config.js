5ffa95f544ba5819b9c41c9f20043e802780a07e
OlegDerevianko <azru2508@gmail.com>
2026-04-08 15:11:27 +0300

    Fix asset paths for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-search-app/', // 👈 Замените на название вашего репозитория
+  base: './', // 👈 Замените на название вашего репозитория
 });
